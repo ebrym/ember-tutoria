6399c4a29b626d03ceb49ff59e6caae8db4042fd
ibrahim <ibrodex@gmail.com>
2021-09-10 13:12:23 +0100

    Initial

---
+++ app/controllers/application.js
@@ -0,0 +1,12 @@
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import Controller from "@ember/controller";
+
+export default class ApplicationController extends Controller {
+  @service session;
+
+  @action
+  invalidateSession() {
+    this.session.invalidate();
+  }
+}
---
+++ app/controllers/login.js
@@ -0,0 +1,22 @@
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import Controller from "@ember/controller";
+import { tracked } from "@glimmer/tracking";
+
+export default class LoginController extends Controller {
+  @service session;
+  @tracked errorMessage = null;
+
+  @action
+  authenticate(event) {
+    event.preventDefault();
+
+    console.log("username = " +  this.identification);
+    console.log("password = " +  this.password);
+    this.session
+      .authenticate("authenticator:oauth2", this.identification, this.password)
+      .catch((reason) => {
+        this.errorMessage = reason.error || reason;
+      });
+  }
+}
